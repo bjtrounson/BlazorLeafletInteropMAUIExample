9880273f5f702a3b343c8fbf5ff09f1558851acf
Benjamin Trounson <btrounson@outlook.co.nz>
2023-12-16 00:42:24 +1300

    Add project files.

---
+++ BlazorLeafletMauiTesting/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BlazorLeafletMauiTesting
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
